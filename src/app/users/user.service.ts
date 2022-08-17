07e55e1160807f9018f7f1353b261fa8f41d5b5b
MarioAbdelmalek <mario_beshai@yahoo.com>
2022-08-17 10:00:19 +0200

    Authentication Done

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -6,14 +6,13 @@ import { Observable } from 'rxjs';
 export class UserService {
 
     constructor(private http: HttpClient) {
-        // move initialization code to ngOnInit. Don't forget the import and implements
     }
 
     registerUser(data: any) {
         return this.http.post('https://localhost:44315/api/User/register', data);
     }
 
-    loginUser(data: any) : Observable<any> {
+    loginUser(data: any): Observable<any> {
         return this.http.post('https://localhost:44315/api/User/authenticate', data, { withCredentials: true });
     }
 }
